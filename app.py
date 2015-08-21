49942c896319ebfcd7ed1cf527452028479d7f06
Christian <christian.etpr10@gmail.com>
2015-08-21 08:59:43 -0700

    Remove debugger

--- app.py
+++ app.py
@@ -25,14 +25,10 @@ def hello_monkey():
     """ % email
 
     r = requests.post('https://api2.autopilothq.com/v1/trigger/0001/contact', data=data, headers=headers)
-    import pdb; pdb.set_trace()
     print email
     print r.json()
     resp = twilio.twiml.Response()
     resp.message("Thanks for sending your email address. We should send you an email shortly")
-
-
-
     return str(resp)
 
 if __name__ == "__main__":
